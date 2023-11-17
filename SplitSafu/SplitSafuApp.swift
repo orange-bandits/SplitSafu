586b6b7e99e8b1cb2a2e03a08280e539813e6798
Tukan van der Borgh <tukanvdborgh@proton.me>
2023-11-17 22:25:54 +0100

    Initial Commit

---
+++ SplitSafu/SplitSafuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitSafuApp.swift
+//  SplitSafu
+//
+//  Created by Tukan van der Borgh on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SplitSafuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
